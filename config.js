6b20e7625efcff23ac1138cde562b44288afafde
Chamod Kaushalya Athapatthu <116275279+athapatthu@users.noreply.github.com>
2023-05-15 22:17:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94743661090', 'sandaru', true],
+  ['94743661090', 'Asrh', true], 
   [''] 
 ] //Number of owners
 
